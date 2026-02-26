baac7a518d650f0a29ed8d44dd7c8855b0a85529
pleniterballet-pixel <pleniterballet@gmail.com>
2026-02-26 09:19:09 -0300

    Correct PROD calculation in desafio_1004.py
    Fix calculation of PROD to use multiplication instead of addition.

--- desafio_1004.py
+++ desafio_1004.py
@@ -1,17 +1,4 @@
-"""
-Beecrowd 1004 - Produto Simples
-
-Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores
-e atribua esta operação à variável PROD. A seguir mostre a variável PROD com
-mensagem correspondente.
-
-Entrada: O arquivo de entrada contém 2 valores inteiros.
-
-Saída: Imprima a mensagem "PROD" conforme exemplo abaixo, com um espaço em branco
-antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto,
-caso contrário seu programa apresentará a mensagem: "Presentation Error".
-"""
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1004
-
-# Escreva sua solução abaixo
+a = int(input("Digite o 1º número"))
+b = int(input("Digite o 2º número"))
+PROD = a+b
+print(f"PROD = {PROD}")
