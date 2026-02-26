831a944b06a01859694b8c9c465c54b46bca83b4
pleniterballet-pixel <pleniterballet@gmail.com>
2026-02-26 09:16:43 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -1,17 +1,4 @@
-"""
-Beecrowd 1003 - Soma Simples
-
-Leia dois valores inteiros, no caso para variáveis A e B.
-A seguir, calcule a soma entre elas e atribua à variável SOMA.
-A seguir escrever o valor desta variável.
-
-Entrada: O arquivo de entrada contém 2 valores inteiros.
-
-Saída: Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço
-em branco antes e depois da igualdade seguido pelo valor correspondente à soma
-de A e B.
-"""
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
-
-# Escreva sua solução abaixo
+a = int(input("Digite o 1ºn inteiro")
+b = int(input("Digite o 2ºn inteiro")
+SOMA = a+b
+print(f"SOMA = {SOMA}")
