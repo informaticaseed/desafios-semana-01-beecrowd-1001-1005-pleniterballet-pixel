09a8434b340249282d33cae34a39849bc9d98a1f
pleniterballet-pixel <pleniterballet@gmail.com>
2026-02-27 19:42:46 -0300

    Refactor input prompts and variable names for clarity

--- desafio_1001.py
+++ desafio_1001.py
@@ -1,4 +1,4 @@
-A = int(input("Digite o 1º número"))
-B = int(input("Digite o 2º número"))
-X = A+B
-print(f"X = {X}")
+a = int(input("Digite o 1º número: "))
+b = int(input("Digite o 2º número: "))
+x = a+b
+print(f"X = {x}")
