169117ad52ee3f9463cbab364eccd267fea6c8f9
pleniterballet-pixel <pleniterballet@gmail.com>
2026-02-27 13:26:48 -0300

    desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -1,4 +1,4 @@
-a = int(input("Digite o 1ºn inteiro")
-b = int(input("Digite o 2ºn inteiro")
+a = int(input("Digite o 1ºn inteiro"))
+b = int(input("Digite o 2ºn inteiro"))
 SOMA = a+b
 print(f"SOMA = {SOMA}")
