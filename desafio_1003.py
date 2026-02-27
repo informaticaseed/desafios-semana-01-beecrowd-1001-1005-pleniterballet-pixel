dc97f654bc504690f44a51bf8197db55cd9a1a78
pleniterballet-pixel <pleniterballet@gmail.com>
2026-02-27 19:46:26 -0300

    desafio_1003.py-Fix input prompts and variable case for sum

--- desafio_1003.py
+++ desafio_1003.py
@@ -1,4 +1,4 @@
-a = int(input("Digite o 1ºn inteiro"))
-b = int(input("Digite o 2ºn inteiro"))
-SOMA = a+b
-print(f"SOMA = {SOMA}")
+a = int(input("Digite o 1ºn inteiro: "))
+b = int(input("Digite o 2ºn inteiro: "))
+soma = a+b
+print(f"SOMA = {soma}")
